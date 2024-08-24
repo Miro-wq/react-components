b32add6aaa3c303b9070bf01be73ab4763eca509
Miro <Miro-wq@users.noreply.github.com>
2024-08-24 18:38:20 +0300

    final update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+// import App from './components/App';
+import React from 'react';
+import user from '../../src/data/user.json';
+import data from '../../src/data/data.json';
+import friends from '../../src/data/friends.json';
+import transactions from '../../src/data/transactions.json';
+
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+     <Statistics title="Upload stats" stats={data} />
+     <FriendList friends={friends} />
+     <TransactionHistory items={transactions} />
+    </>
   );
-};
+}
